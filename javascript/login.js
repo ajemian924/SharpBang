91f12c5a49c307a4a9297ccdba1f400aabf91f28
MrBacon470 <MrBacon470@outlook.com>
2025-11-17 14:54:05 -0600

    Functionality Implemented

--- javascript/login.js
+++ javascript/login.js
@@ -1,25 +1,32 @@
 const emailInput = document.getElementById('emailInput')
-const passwordInput = document.getElementById('password')
+const passwordInput = document.getElementById('passwordInput')
 
 function login() {
     const credentials = [
         {email: 'admin@email.com', password: 'password'}
     ]
     const email = emailInput.value
-    const password = passwordInput.value;
+    console.log(email)
+    const password = passwordInput.value
+    console.log(password)
 
-    credentials.forEach((element) => {
-        if(email === element.email && password === element.password) {
+    for(let i = 0; i < credentials.length; i++) {
+        if(email == credentials[i].email && password == credentials[i].password) {
             return true
         }
-    })
+    }
     return false
 }
 
 document.getElementById('submitCredentialsButton').addEventListener('click', () => {
     const result = login()
+
     if(result) {
         document.getElementById('loginWindow').style.display = 'none'
         document.getElementById('programWindow').style.display = 'block'
     }
+    else {
+        document.getElementById('feedbackText').innerText = 'Username and/or Password is Incorrect'
+    }
+
 })
