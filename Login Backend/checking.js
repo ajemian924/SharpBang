abbd6ff804cee16b611956e4634879aa6bc2b02a
banana2886 <vule2886@gmail.com>
2025-11-20 23:10:31 -0600

    Add files via upload

---
+++ Login Backend/checking.js
@@ -0,0 +1,20 @@
+class Checking {
+	constructor() {
+        document.querySelector("body").style.display = "none";
+		const auth = localStorage.getItem("auth");
+		this.validateAuth(auth);
+	}
+
+	validateAuth(online) {
+		if (online != 1) {
+			window.location.replace("/");
+		} else {
+            document.querySelector("body").style.display = "block";
+		}
+	}
+
+	logOut() {
+		localStorage.removeItem("auth");
+		window.location.replace("/");
+	}
+}
