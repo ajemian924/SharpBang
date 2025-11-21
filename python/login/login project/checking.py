dacae93d3a07806989acfe851a43820aa201ab5c
banana2886 <vule2886@gmail.com>
2025-11-20 22:47:21 -0600

    Delete python/login/login project directory

--- python/login/login project/checking.py
+++
@@ -1,5 +0,0 @@
-def status_logic(request):
-    if request.user.is_authenticated:
-        return JsonResponse({"authenticated": True, "username": request.user.username})
-    else:
-        return JsonResponse({"authenticated": False})
--- python/login/login project/logout.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import logout
-
-@csrf_exempt
-def logout_logic(request):
-    logout(request)
-    return JsonResponse({"message": "Logged out"})
